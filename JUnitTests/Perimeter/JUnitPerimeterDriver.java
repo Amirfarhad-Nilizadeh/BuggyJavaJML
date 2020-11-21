dd65c8463521638f9a60077a28ac4363086609b5
Amirfarhad-Nilizadeh <amirfarhad.nilizadeh@gmail.com>
2020-11-21 18:05:15 -0500

    Update

--- JUnitTests/Perimeter/JUnitPerimeterDriver.java
+++ JUnitTests/Perimeter/JUnitPerimeterDriver.java
@@ -7,180 +7,180 @@ public class JUnitPerimeterDriver {
 	@Test
 	public void test() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(4346580746L, a.Driver(3, 543781664, 1629508709, 1936990306, 2032157037, (short)26994, 7377303457245700174L));
+		assertEquals(4346580746L, a.driver(3, 543781664, 1629508709, 1936990306, 2032157037, (short)26994, 7377303457245700174L));
 	}
 
 	@Test
 	public void test1() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(4032900904L, a.Driver(5, 543236095, 1933212, 1652109095, 1835622502, (short)24946, 7503268628606315625L));
+		assertEquals(4032900904L, a.driver(5, 543236095, 1933212, 1652109095, 1835622502, (short)24946, 7503268628606315625L));
 	}
 
 	@Test
 	public void test2() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(1456, a.Driver(0, 15040659, 25366528, 442862338, 65645, (short)364, 2517043135789139455L));
+		assertEquals(1456, a.driver(0, 15040659, 25366528, 442862338, 65645, (short)364, 2517043135789139455L));
 	}
 
 	@Test
 	public void test3() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(1718052328, a.Driver(4, 858993460, 859045760, 13108, 858993460, (short)23, 72103773526294528L));
+		assertEquals(1718052328, a.driver(4, 858993460, 859045760, 13108, 858993460, (short)23, 72103773526294528L));
 	}
 
 	@Test
 	public void test4() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(5793246152107622394L, a.Driver(2, 1769611369, 1932032544, 2138253567, 8388353, (short)27392, 965541025351270399L));
+		assertEquals(5793246152107622394L, a.driver(2, 1769611369, 1932032544, 2138253567, 8388353, (short)27392, 965541025351270399L));
 	}
 
 	@Test
 	public void test5() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(9178135550L, a.Driver(1, 1835627110, 1634887777, 1828716288, 8388449, (short)29286, 7021789536673857280L));
+		assertEquals(9178135550L, a.driver(1, 1835627110, 1634887777, 1828716288, 8388449, (short)29286, 7021789536673857280L));
 	}
 
 	@Test
 	public void test6() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(86420, a.Driver(0, 1313754673, 151606376, 1769152617, 1931501856, (short)21605, 8319310018971189613L));
+		assertEquals(86420, a.driver(0, 1313754673, 151606376, 1769152617, 1931501856, (short)21605, 8319310018971189613L));
 	}
 
 	@Test
 	public void test7() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(1925890818, a.Driver(4, 5131825, 151606376, 1769152617, 1931501856, (short)256, 432344488453586579L));
+		assertEquals(1925890818, a.driver(4, 5131825, 151606376, 1769152617, 1931501856, (short)256, 432344488453586579L));
 	}
 
 	@Test
 	public void test8() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(4968659480L, a.Driver(1, 993731896, 989863995, 20659113, 1457060056, (short)10024, 4268069943049715712L));
+		assertEquals(4968659480L, a.driver(1, 993731896, 989863995, 20659113, 1457060056, (short)10024, 4268069943049715712L));
 	}
 
 	@Test
 	public void test9() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(5149631490L, a.Driver(5, 1297370673, 151606376, 1769152585, 1931501856, (short)21588, 5642533480878901588L));
+		assertEquals(5149631490L, a.driver(5, 1297370673, 151606376, 1769152585, 1931501856, (short)21588, 5642533480878901588L));
 	}
 
 	@Test
 	public void test10() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(204993338463557280L, a.Driver(2, 1702056512, 7928129, 1836480118, 2088138358, (short)8192, 34165556410592880L));
+		assertEquals(204993338463557280L, a.driver(2, 1702056512, 7928129, 1836480118, 2088138358, (short)8192, 34165556410592880L));
 	}
 
 	@Test
 	public void test11() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(5285518848L, a.Driver(3, 1312161712, 1330597712, 1330597712, 1330597712, (short)20480, 7596562821896995327L));
+		assertEquals(5285518848L, a.driver(3, 1312161712, 1330597712, 1330597712, 1330597712, (short)20480, 7596562821896995327L));
 	}
 
 	@Test
 	public void test12() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(3536741480L, a.Driver(1, 707348296, 1313425993, 1213089863, 623074854, (short)10346, 7740399544290595944L));
+		assertEquals(3536741480L, a.driver(1, 707348296, 1313425993, 1213089863, 623074854, (short)10346, 7740399544290595944L));
 	}
 
 	@Test
 	public void test13() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(5685788870L, a.Driver(3, 712188002, 2130706433, 7170395, 1717662312, (short)8249, 7596562145956332913L));
+		assertEquals(5685788870L, a.driver(3, 712188002, 2130706433, 7170395, 1717662312, (short)8249, 7596562145956332913L));
 	}
 
 	@Test
 	public void test14() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(2803848442L, a.Driver(4, 543324448, 542861682, 1717662312, 2038327649, (short)29288, 7017789232913279232L));
+		assertEquals(2803848442L, a.driver(4, 543324448, 542861682, 1717662312, 2038327649, (short)29288, 7017789232913279232L));
 	}
 
 	@Test
 	public void test15() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(3856269865L, a.Driver(5, 707348296, 1313425993, 1213078599, 622416977, (short)26995, 2330189636053706078L));
+		assertEquals(3856269865L, a.driver(5, 707348296, 1313425993, 1213078599, 622416977, (short)26995, 2330189636053706078L));
 	}
 
 	@Test
 	public void test16() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(55776, a.Driver(0, 502205062, 1073759608, 2046820474, 16785408, (short)13944, 8791045117638082614L));
+		assertEquals(55776, a.driver(0, 502205062, 1073759608, 2046820474, 16785408, (short)13944, 8791045117638082614L));
 	}
 
 	@Test
 	public void test17() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(434041011258261504L, a.Driver(2, 1862205183, 18155521, 2147483392, 256001, (short)257, 72340168543043584L));
+		assertEquals(434041011258261504L, a.driver(2, 1862205183, 18155521, 2147483392, 256001, (short)257, 72340168543043584L));
 	}
 
 	@Test
 	public void test18() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(4783675666L, a.Driver(3, 1751540788, 640297045, 1246185042, 625369656, (short)14647, 8373943436225305450L));
+		assertEquals(4783675666L, a.driver(3, 1751540788, 640297045, 1246185042, 625369656, (short)14647, 8373943436225305450L));
 	}
 
 	@Test
 	public void test19() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(1175, a.Driver(1, 235, 653713931, 1545320878, 626746111, (short)32567, 8358680910554095712L));
+		assertEquals(1175, a.driver(1, 235, 653713931, 1545320878, 626746111, (short)32567, 8358680910554095712L));
 	}
 
 	@Test
 	public void test20() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(3541289202L, a.Driver(4, 1330315126, 794845193, 1416128883, 543781664, (short)24864, 6081393814430447904L));
+		assertEquals(3541289202L, a.driver(4, 1330315126, 794845193, 1416128883, 543781664, (short)24864, 6081393814430447904L));
 	}
 
 	@Test
 	public void test21() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(5632, a.Driver(0, 553546611, 1785358976, 1672416000, 33161217, (short)1408, 333266372427516928L));
+		assertEquals(5632, a.driver(0, 553546611, 1785358976, 1672416000, 33161217, (short)1408, 333266372427516928L));
 	}
 
 	@Test
 	public void test22() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(774381612, a.Driver(5, 168428043, 168430091, 269093387, 168430091, (short)2571, 723609297868854680L));
+		assertEquals(774381612, a.driver(5, 168428043, 168430091, 269093387, 168430091, (short)2571, 723609297868854680L));
 	}
 
 	@Test
 	public void test23() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(4033685368728614400L, a.Driver(2, 67134512, 1195790885, 1179006501, 1180842041, (short)24680, 672280894788102400L));
+		assertEquals(4033685368728614400L, a.driver(2, 67134512, 1195790885, 1179006501, 1180842041, (short)24680, 672280894788102400L));
 	}
 
 	@Test
 	public void test24() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(4783675666L, a.Driver(3, 1751540788, 640297045, 1246185042, 625369656, (short)14647, 8373943436225305450L));
+		assertEquals(4783675666L, a.driver(3, 1751540788, 640297045, 1246185042, 625369656, (short)14647, 8373943436225305450L));
 	}
 
 	@Test
 	public void test25() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(10183767300L, a.Driver(1, 2036753460, 2116681760, 14766376, 2097377, (short)20839, 7377303822317938278L));
+		assertEquals(10183767300L, a.driver(1, 2036753460, 2116681760, 14766376, 2097377, (short)20839, 7377303822317938278L));
 	}
 
 	@Test
 	public void test26() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(3006472754L, a.Driver(5, 286327039, 2147483409, 286331153, 286331153, (short)4369, 1229782938247303441L));
+		assertEquals(3006472754L, a.driver(5, 286327039, 2147483409, 286331153, 286331153, (short)4369, 1229782938247303441L));
 	}
 
 	@Test
 	public void test27() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(12340, a.Driver(0, 185666573, 202116141, 202116109, 202116109, (short)3085, 868082074560236560L));
+		assertEquals(12340, a.driver(0, 185666573, 202116141, 202116109, 202116109, (short)3085, 868082074560236560L));
 	}
 
 	@Test
 	public void test28() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(3756762859L, a.Driver(4, 1217740130, 1897939252, 641083477, 1246185042, (short)18550, 4423222021352941928L));
+		assertEquals(3756762859L, a.driver(4, 1217740130, 1897939252, 641083477, 1246185042, (short)18550, 4423222021352941928L));
 	}
 
 	@Test
 	public void test29() {
 		PerimeterDriver a = new PerimeterDriver();
-		assertEquals(3087453224394L, a.Driver(2, 1751574760, 387389325, 560558888, 911500659, (short)29796, 514575537399L));
+		assertEquals(3087453224394L, a.driver(2, 1751574760, 387389325, 560558888, 911500659, (short)29796, 514575537399L));
 	}
 }
