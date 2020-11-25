a1734bbdbe5a1c1f750d565206acb1a4bb20276f
Amirfarhad-Nilizadeh <amirfarhad.nilizadeh@gmail.com>
2020-11-25 15:09:43 -0500

    Update

--- BuggyJava+JML/CopyArray/bug1/CopyArray.java
+++ BuggyJava+JML/CopyArray/bug1/CopyArray.java
@@ -1,8 +1,8 @@
 
 public class CopyArray {
-    //@ requires 0 < a.length && a.length <= Integer.MAX_VALUE && 0 < b.length && b.length <= Integer.MAX_VALUE;
-    //@ requires a.length == b.length;
-    //@ requires 0 <= iBegin && iBegin < a.length && 0 <= iEnd && iEnd < a.length && iBegin <= iEnd;
+    //@ requires 0 < a.length && 0 < b.length;
+    //@ requires 0 <= iBegin && 0 <= iEnd && iBegin <= iEnd;
+    //@ requires iBegin < a.length && iBegin < b.length && iEnd < a.length && iEnd < b.length;
     //@ ensures (\forall int i; iBegin <= i && i < iEnd; a[i] == b[i]);
     public static void CopyArray(int[] b, int iBegin, int iEnd, int[] a) {
         int k = iBegin;
